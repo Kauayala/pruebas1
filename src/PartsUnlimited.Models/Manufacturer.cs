7ce19dbd7cd64ac1dfcd2aa098df5aea386afb68
Kuayala <creyes@dian.gov.co>
2024-05-17 13:37:20 -0500

    primera prueba de la tarde

--- src/PartsUnlimited.Models/Manufacturer.cs
+++ src/PartsUnlimited.Models/Manufacturer.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// cambios de arquitect A
 using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
--- src/PartsUnlimited.Models/Raincheck.cs
+++ src/PartsUnlimited.Models/Raincheck.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+//cambios de arquitect B
 namespace PartsUnlimited.Models
 {
     public class Raincheck
