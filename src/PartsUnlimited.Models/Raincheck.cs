8fc498540230207806717a4f6c65142aedb78197
Kuayala <creyes@dian.gov.co>
2024-05-17 13:39:03 -0500

    segundos cambios de la tarde

--- src/PartsUnlimited.Models/Raincheck.cs
+++ src/PartsUnlimited.Models/Raincheck.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// cambios de arquitect D
 namespace PartsUnlimited.Models
 {
     public class Raincheck
